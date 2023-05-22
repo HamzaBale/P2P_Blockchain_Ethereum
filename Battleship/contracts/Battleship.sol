d1960ee7d36afff596a3da58aafac5ccdbacca49
HamzaBale <hamzacod9@gmail.com>
2023-05-22 08:17:34 +0200

    Add Amount to spend and decide

--- Battleship/contracts/Battleship.sol
+++ Battleship/contracts/Battleship.sol
@@ -2,46 +2,87 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Battleship {
-   struct Game {
+  //struct for the Game
+  struct Game {
         address first;
         address second;
+        uint amount;
+        uint tempAmount;
+        
   }
+  //event to send when game starts
+  event GameStarted(address indexed _first,address indexed _second, uint indexed _gameId);
+  //event to declare the amount of money the game will have, it is used during the decision of how much money to spend in the game
+  event AmountToSpend(uint indexed _gameId,uint _amount);
+  //event for the amount that the game will have
+  event AmountDecided(uint indexed _gameId,uint _amount);
+
+  //Array of games
   Game[] public listOfGames;
+  //variable to keep the number of open games.
   uint public openGames = 0;
   constructor() {
   }
 
   function CreateGame() public returns (uint) { //Game Creation, creates a game with only the first player saved.
-    listOfGames.push(Game(msg.sender,address(0)));
+    listOfGames.push(Game(msg.sender,address(0),0,0));
     openGames++;
     return listOfGames.length - 1; 
   }
-  function JoinGame(bool isRand,uint gameIndex) public returns (uint){//Join a game.
+  function JoinGame(bool _isRand,uint _gameIndex) public returns (uint){//Join a game.
     if(openGames <= 0) revert("No open games");
-    if(!isRand) { //can be joined specifically by passing a game id.
-      if(listOfGames[gameIndex].second != address(0)) revert("Game is not Free");
-      listOfGames[(gameIndex)].second = msg.sender;
+    
+    bool found = false;
+    uint returnIndex = 0;
+    if(!_isRand) { //can be joined specifically by passing a game id.
+      require(_gameIndex < listOfGames.length);
+      returnIndex = _gameIndex;
+      if(listOfGames[_gameIndex].second != address(0)) revert("Game is not Free");
+      listOfGames[(_gameIndex)].second = msg.sender;
       openGames--;
-      return (gameIndex);
+      found = true;
     }
     //can be joined randomly, the contract will find a random game from the open ones.
-    bytes32 rand = randGenerator();
-    bool found = false;
-    uint index = uint(rand) % openGames + 1;
-    uint returnIndex = 0;
-    for(uint i = 0; i < listOfGames.length; i++){
-      if(listOfGames[i].second == address(0) && index > 1) index--;
-      else if (listOfGames[i].second == address(0) && index == 1) {
-        listOfGames[i].second = msg.sender;
-        returnIndex = i;
-        openGames--;
-        found = true;
-        break;
+
+    if(_isRand && !found){
+      bytes32 rand = randGenerator();
+      uint index = uint(rand) % openGames + 1;
+      for(uint i = 0; i < listOfGames.length; i++){
+        if(listOfGames[i].second == address(0) && index > 1) index--;
+        else if (listOfGames[i].second == address(0) && index == 1) {
+          listOfGames[i].second = msg.sender;
+          returnIndex = i;
+          openGames--;
+          found = true;
+          break;
+        }
       }
     }
+
+    if(found) {
+      emit GameStarted(listOfGames[returnIndex].first,listOfGames[returnIndex].second,returnIndex);
+      }
+
     if(!found) revert("Not able to find an open game");
     return returnIndex;
   }
+
+  function amountCommit(uint _gameId,uint _amount) public{
+    require(_amount > 0 && _gameId < listOfGames.length && listOfGames[_gameId].first != address(0) && listOfGames[_gameId].second != address(0));
+    require(listOfGames[_gameId].first == msg.sender || listOfGames[_gameId].second == msg.sender);
+    if(listOfGames[_gameId].amount != 0) revert("The amount for this game has already been decided");
+    listOfGames[_gameId].tempAmount = _amount;
+    emit AmountToSpend(_gameId, _amount);
+  }
+  function acceptedAmount(uint _gameId) public {
+    bool condition = _gameId < listOfGames.length && listOfGames[_gameId].first != address(0) && listOfGames[_gameId].second != address(0) && (listOfGames[_gameId].first == msg.sender || listOfGames[_gameId].second == msg.sender);
+    require(condition);
+    if(listOfGames[_gameId].amount != 0) revert("The amount for this game has already been decided!");
+    if(listOfGames[_gameId].tempAmount == 0) revert("The amount for this game wasn't decided yet!");
+    listOfGames[_gameId].amount = listOfGames[_gameId].tempAmount;
+    emit AmountDecided(_gameId,listOfGames[_gameId].amount);
+  }
+
 
   function randGenerator() private view returns (bytes32){
     //Helps with random number generation
