55ebf002b7aedd700b8b581979db3430dd135ad5
HamzaBale <hamzacod9@gmail.com>
2023-05-23 19:04:08 +0200

    New Game Done

--- Battleship/contracts/Battleship.sol
+++ Battleship/contracts/Battleship.sol
@@ -6,20 +6,24 @@ contract Battleship {
   struct Game {
         address first;
         address second;
+        uint boardDim;
         uint amount;
         address amountRequester;//used to don't allow a user to commit an amount and accept it all by himself.
         uint tempAmount;
         string merkle_first;
         string merkle_second;
+
   }
   //event to send when game starts
-  event GameStarted(address indexed _first,address indexed _second, uint indexed _gameId);
+  event GameStarted(address indexed _first,address indexed _second,uint _amountTemp ,uint indexed _gameId,uint _boardDim);
   //event to declare the amount of money the game will have, it is used during the decision of how much money to spend in the game
-  event AmountToSpend(uint indexed _gameId,uint _amount);
+  event AmountToSpend(uint indexed _gameId,uint _amount,address _from);
   //event for the amount that the game will have
   event AmountDecided(uint indexed _gameId,uint _amount);
   //event for outputting an uint value
   event UintOutput(address indexed _from,uint _value);
+  //ErrorOut string
+  error ErrorOut(string err);
 
   //Array of games
   Game[] public listOfGames;
@@ -28,21 +32,22 @@ contract Battleship {
   constructor() {
   }
 
-  function CreateGame() public{ //Game Creation, creates a game with only the first player saved.
-    listOfGames.push(Game(msg.sender,address(0),0,address(0),0,"",""));
+  function CreateGame(uint _boardDim) public{ //Game Creation, creates a game with only the first player saved.
+    listOfGames.push(Game(msg.sender,address(0),_boardDim,0,address(0),0,"",""));
     openGames++;
     emit UintOutput(msg.sender,listOfGames.length - 1); 
   }
-  function JoinGame(bool _isRand,uint _gameIndex) public{//Join a game.
-    if(openGames <= 0) revert("No open games");
+  function JoinGame(bool _isRand,uint _gameIndex) public {//Join a game.
+    if(openGames <= 0) revert ErrorOut({err:"No open games"});
     
     bool found = false;
     uint returnIndex = 0;
     if(!_isRand) { //can be joined specifically by passing a game id.
-      require(_gameIndex < listOfGames.length);
+      require(_gameIndex < listOfGames.length,"Something went wrong!");
       returnIndex = _gameIndex;
-      if(listOfGames[returnIndex].first == msg.sender) revert("User can't access his own game!");
-      if(listOfGames[returnIndex].second != address(0)) revert("Game is not Free");
+      if(listOfGames[returnIndex].first == address(0)) revert ErrorOut({err:"You can't join this game, because there's no player here!"});
+      //if(listOfGames[returnIndex].first == msg.sender) revert("User can't access his own game!");
+      if(listOfGames[returnIndex].second != address(0)) revert ErrorOut({err:"Game is not Free"});
       listOfGames[(returnIndex)].second = msg.sender;
       openGames--;
       found = true;
@@ -53,8 +58,8 @@ contract Battleship {
       bytes32 rand = randGenerator();
       uint index = uint(rand) % openGames + 1;
       for(uint i = 0; i < listOfGames.length; i++){
-        if(listOfGames[i].second == address(0) && index > 1) index--;
-        else if (listOfGames[i].second == address(0) && index == 1) {
+        if(listOfGames[i].second == address(0) && listOfGames[i].first != address(0) && index > 1) index--;
+        else if (listOfGames[i].second == address(0) && listOfGames[i].first != address(0) && index == 1) {
           listOfGames[i].second = msg.sender;
           returnIndex = i;
           openGames--;
@@ -64,28 +69,36 @@ contract Battleship {
       }
     }
 
-    if(found) emit GameStarted(listOfGames[returnIndex].first,listOfGames[returnIndex].second,returnIndex);
+    if(found) emit GameStarted(listOfGames[returnIndex].first,listOfGames[returnIndex].second,listOfGames[returnIndex].tempAmount,returnIndex,listOfGames[returnIndex].boardDim);
       
-    if(!found) revert("Not able to find an open game");
+    if(!found)revert ErrorOut({err:"Not able to find an open game"});
   }
 
   function AmountCommit(uint _gameId,uint _amount) public{
-    require(_amount > 0 && _gameId < listOfGames.length && listOfGames[_gameId].first != address(0) && listOfGames[_gameId].second != address(0));
-    require(listOfGames[_gameId].first == msg.sender || listOfGames[_gameId].second == msg.sender);
-    if(listOfGames[_gameId].amount != 0) revert("The amount for this game has already been decided");
+    if(listOfGames[_gameId].first == address(0)) revert ErrorOut({err:"The creator id is null"});
+    require(_amount > 0 && _gameId < listOfGames.length,"The amount or the gameId are null!");
+    require(listOfGames[_gameId].first == msg.sender || listOfGames[_gameId].second == msg.sender,"this user is not part of the game!");
+    if(listOfGames[_gameId].amount != 0) revert ErrorOut({err:"The amount for this game has already been decided"});
     listOfGames[_gameId].tempAmount = _amount;
     listOfGames[_gameId].amountRequester = msg.sender;
-    emit AmountToSpend(_gameId, _amount);
+    emit AmountToSpend(_gameId, _amount,msg.sender);
   }
   function AcceptedAmount(uint _gameId) public {
     bool condition = _gameId < listOfGames.length && listOfGames[_gameId].first != address(0) && listOfGames[_gameId].second != address(0) && (listOfGames[_gameId].first == msg.sender || listOfGames[_gameId].second == msg.sender);
-    require(condition);
-    require(listOfGames[_gameId].amountRequester != address(0) && listOfGames[_gameId].amountRequester != msg.sender);
-    if(listOfGames[_gameId].amount != 0) revert("The amount for this game has already been decided!");
-    if(listOfGames[_gameId].tempAmount == 0) revert("The amount for this game wasn't decided yet!");
+    require(condition,"Something went wrong!");
+    require(listOfGames[_gameId].amountRequester != address(0) && listOfGames[_gameId].amountRequester != msg.sender,"Something went wrong!");
+    if(listOfGames[_gameId].amount != 0) revert ErrorOut({err:"The amount for this game has already been decided!"});
+    if(listOfGames[_gameId].tempAmount == 0) revert ErrorOut({err:"The amount for this game wasn't decided yet!"});
     listOfGames[_gameId].amount = listOfGames[_gameId].tempAmount;
     emit AmountDecided(_gameId,listOfGames[_gameId].amount);
   }
+
+  function LeaveGame(uint _gameId) public { //you can leave the game only if you are the creator and before the game has started.
+    require(_gameId < listOfGames.length && listOfGames[_gameId].first == msg.sender && listOfGames[_gameId].second == address(0),"You can't leave the game if you're not the owner!");
+    listOfGames[_gameId].first = address(0);
+    openGames--;
+  }
+
 
   //TO DO START GAME, CONTROLLO CHE AMOUNT è STATO DECISO
 
--- Battleship/src/js/app.js
+++ Battleship/src/js/app.js
@@ -1,25 +1,11 @@
+var gameId = null;
+var gameAmount = null;
+var boardDim = null;
 App = {
   web3Provider: null,
   contracts: {},
   
   init: async function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
-
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-
-        petsRow.append(petTemplate.html());
-      }
-    });
-
     return await App.initWeb3();
   },
 
@@ -45,78 +31,287 @@ App = {
   },
 
   initContract: function() {
-    $.getJSON("Battleship.json", function(data) {
-    var BattleshipArtifact = data; // Get the contract artifact and  initialize it
+    $.getJSON("Battleship.json",  function(data) {
+     BattleshipArtifact = data; // Get the contract artifact and  initialize it
     App.contracts.Battleship = TruffleContract(BattleshipArtifact);
     // Set the web3.js provider for our contract to the provider defined in the previous function
     App.contracts.Battleship.setProvider(App.web3Provider);
     // Use the contract to retrieve and mark the adopted pets
     //return App.initBoard();
+    
     });
     return App.bindEvents();
     },
 
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
+  bindEvents: async function() {
+    
+    //Game creation and joining
     $(document).on('click', '#CreateGameBtn', App.CreateGame);
     $(document).on('click', '#JoinGameIdBtn', App.JoinGameId);
+    $(document).on('click', '#JoinRandGameBtn', App.JoinRandomGame);
+    $(document).on('input',"#BoardDim", (event) => boardDim = event.target.value);
+    //Go Back to main menu button
+    $(document).on('click', '#GoBackToMainBtn', App.LeaveGame);
+    //Amount of money to send in order to decide how much to bet.
+    $(document).on('input', '#AmountToSend', App.UnlockButtons);
+    //Accept the amount of money set
+    $(document).on('click', '#AcceptAmount', App.AcceptAmount);
+    //To set new amount of money
+    $(document).on('click', '#NewAmountBtn', App.AmountCommit);
+    $(document).on('input', '#NewAmount', () => { gameAmount = $('#NewAmount').val(); AmountCommit()});
+
+
+    $(document).on('click',"#xxx",App.CreateTable);
+    
+    
+
+    $('#CreateGameBtn').prop("disabled", true);
+
+  
+    
     
   },
 
   initBoard: function() {
     
   },
+  UnlockButtons: function(){
+    gameAmount = $('#AmountToSend').val();
+    if(gameAmount){
+      $('#CreateGameBtn').prop("disabled", false);
 
-  CreateGame: function(){
+    } else {
+      $('#CreateGameBtn').prop("disabled", true);
+    }
 
+  },
+  AmountCommit: function(){
+    console.log(gameId +"  "+ gameAmount);
+    if(!gameAmount || gameId == null || gameId < 0) return alert("Game amount or game id are null!");
+    return App.contracts.Battleship.deployed().then(function (instance) {
+      battleshipInstance = instance;
+      console.log(gameId,parseInt(gameAmount));
+       return battleshipInstance.AmountCommit(gameId,parseInt(gameAmount));
+      }).then(function (reciept){
+        alert("You sent the following "+ gameAmount+". You need to wait for your opponent to accept this amount!");
+      }).catch(function(err) {
+        console.log(err);
+          throw new Error(err);
+        })
+  },
+  AcceptAmount: function(){
+    
+    //GO TO BOARD
     App.contracts.Battleship.deployed().then(function (instance) {
+      battleshipInstance = instance;
+       return battleshipInstance.AcceptedAmount(gameId);
+      }).then(function (reciept){
+        console.log(reciept);
+        $('#GameBoard').show();
+        $('#AmountValidation').hide();
+        App.CreateTable();
+
+    
+      }).catch(function(err) {
+        console.log(err);
+          throw new Error(err);
+        })
+
+
+
+  },
+
+
+
+  CreateGame:  function(){
+
+    if(!boardDim) return alert("You must choose a board dimension");
+
+    App.contracts.Battleship.deployed().then(async function (instance) {
         battleshipInstance = instance;
-         return battleshipInstance.CreateGame();
-        }).then(function (reciept){
+
+         return battleshipInstance.CreateGame(boardDim);
+        }).then(async function (reciept){
           var logsArray = reciept.logs;
-          logsArray.forEach(log => {
-            var gameId = log.args._value.toNumber();
+            gameId = logsArray[0].args._value.toNumber();
             console.log(gameId);
             if(gameId < 0) console.error("Something went wrong, game id is negative!");
             else {
+              try {
+                await App.AmountCommit();
+              } catch (error) {
+                return console.log(error);
+              }
               alert("Game Created");
               $('#GameCreationContainer').hide();
+              $('#GameBoardContainer').show();
+              $('#CreationGameTitle').text("Welcome to the waiting room, wait until someone joins! Game ID: "+gameId);
+            const PAST_EVENT_DECIDED = async () => {
+              await battleshipInstance.allEvents("AmountDecided",
+                (err, events) => {
+                  var gameIdTemp = events.args._gameId.toNumber();
+                  if(events.event =="AmountDecided" && gameIdTemp == gameId){
+                  gameAmount = events.args._amount.toNumber();
+                    $("#GameBoardContainer").hide();
+                    alert("Game Started!");
 
+                    $("#GameBoard").show();
+                    $("#AmountValidation").hide();
+                    App.CreateTable();
+                  }
+                });
+            };
+            const PAST_EVENT_COMMIT = async () => {
+              await battleshipInstance.allEvents("AmountToCommit",
+                (err, events) => {
+                  var gameIdTemp = events.args._gameId.toNumber();
+                  var fromAccount = events.args._from;
+            
+                  if(events.event =="AmountToSpend" && gameIdTemp == gameId && fromAccount != web3.eth.defaultAccount){
+                  gameAmount = events.args._amount.toNumber();
+                    alert("The new amount is "+ gameAmount);
+                    $("#GameBoardContainer").hide();
+                    $("#AmountValidation").show();
+                    $("#GameBoardTitle").text("Welcome to the game with ID "+gameId+ ", Choose an amount or accept the one that your oppenent advised. The current amount is "+gameAmount);
+                    
+                    resolve('Operation completed.');
+                  }
+                });
+            };
+            PAST_EVENT_DECIDED();
+            PAST_EVENT_COMMIT();
             }
-          });
+          
           
         }).catch(function(err) {
-             console.error(err.message);
-          });;
-
-
-
+             console.error(err);
+          });
 
   },
 
   JoinGameId: function(){
+    
     var inputValue = $('#GameId').val();
     if(!inputValue) alert("You must select a game ID!");
     else{
-      console.log(inputValue);
-      // TODO JOIN GAME BY ID
+        App.contracts.Battleship.deployed().then(function (instance) {
+          battleshipInstance = instance;
+           return battleshipInstance.JoinGame(false,inputValue);
+          }).then(function (reciept){
+            console.log(reciept);
+            gameId = inputValue;
+            gameAmount = reciept.logs[0].args._amountTemp.toNumber();
+            boardDim = reciept.logs[0].args._boardDim.toNumber();
+            alert("You joined the game, the amount of ETH was set to "+gameAmount);
+            
+            $('#GameCreationContainer').hide();
+            $('#AmountValidation').show();
+            $("#GameBoardTitle").text("Welcome to the game with ID "+gameId+" Choose an amount or accept the one that your oppenent advised. The current amount is "+gameAmount);
+            const PAST_EVENT_DECIDED = async () => {
+              await battleshipInstance.allEvents("AmountDecided",
+                (err, events) => {
+                  var gameIdTemp = events.args._gameId.toNumber();
+                  if(events.event =="AmountDecided" && gameIdTemp == gameId){
+                    gameAmount = events.args._amount.toNumber();
+                    $("#AmountValidation").hide();
+                    alert("Game Started!");
+                    $("#GameBoard").show();
+                    resolve('Operation completed.');
+                  }
+                });
+            };
+            const PAST_EVENT_COMMIT = async () => {
+              await battleshipInstance.allEvents("AmountToCommit",
+                (err, events) => {
+                  var gameIdTemp = events.args._gameId.toNumber();
+                  var fromAccount = events.args._from;
+                  if(events.event =="AmountToSpend" && gameIdTemp == gameId && fromAccount != web3.eth.defaultAccount){
+                  gameAmount = events.args._amount.toNumber();
+                  alert("The new amount is "+ gameAmount);
+                    $("#GameBoardContainer").hide();
+                    $("#AmountValidation").show();
+                    $("#GameBoardTitle").text("Welcome to the game with ID "+gameId+ ", Choose an amount or accept the one that your oppenent advised. The current amount is "+gameAmount);
+
+                    resolve('Operation completed.');
+                  }
+                });
+            };
+            PAST_EVENT_DECIDED();
+            PAST_EVENT_COMMIT();
+            
+
+          }).catch(function(err) {
+               console.log(err);
+
+            })
     }
 
   },
+  JoinRandomGame: function(){
+    App.contracts.Battleship.deployed().then(function (instance) {
+      battleshipInstance = instance;
+       return battleshipInstance.JoinGame(true,0);
+      }).then(function (reciept){
+        console.log(reciept);
+        gameId = reciept.logs[0].args._gameId.toNumber();
+        gameAmount = reciept.logs[0].args._amountTemp.toNumber();
+        alert("You joined the game, the amount of ETH was set to "+gameAmount);
+        
+        $('#GameCreationContainer').hide();
+        $('#AmountValidation').show();
+        $("#GameBoardTitle").text("Welcome to the game number "+gameId+", Choose an amount or accept the one that your oppenent advised. The current amount is "+gameAmount);
+        
+      }).catch(function(err) {
+           console.log(err);
 
-  handleAdopt: function(event) {
-    event.preventDefault();
+        })
 
-    var petId = parseInt($(event.target).data('id'));
+  },
 
-    alert(petId);
-    /*
-     * Replace me...
-     */
+
+  CreateTable: function(){
+    var table = document.getElementById("GameTable");
+    var tableArr = [
+  [1, 0, 1],
+  [1, 0, 1],
+  [0, 0, 0]
+  ];
+
+  for(let i = 0; i < tableArr.length; i++){
+    table.insertRow(i);
+    for (let j = 0; j < tableArr[i].length; j++) {
+      const cell = tableArr[i].insertCell(j);
+      cell.textContent = tableArr[i][j];
+    }
   }
 
-  
+  },
 
+
+
+  LeaveGame: function(event) {
+    event.preventDefault();
+    var val = confirm("Are you sure you want to leave the game?");
+    if (val == true) {
+      console.log("You pressed OK.");
+      if(gameId == null) alert("Something went wrong, gameId is null!");
+      else{
+      App.contracts.Battleship.deployed().then(function (instance) {
+        battleshipInstance = instance;
+         return battleshipInstance.LeaveGame(gameId);
+        }).then(function (reciept){
+          console.log(reciept);
+          $('#GameCreationContainer').show();
+          $('#GameBoardContainer').hide();
+          
+        }).catch(function(err) {
+             console.error(err);
+          });
+
+      }
+    }
+
+  }
 };
 
 $(function() {
